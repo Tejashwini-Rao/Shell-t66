769699eab616e00e082eb6149c1a620c38cb6ee3
Tejashwini Rao <polepalle.07@gmail.com>
2023-04-26 21:02:52 +0530

    Pushing all the components

--- common.sh
+++ common.sh
@@ -1,9 +1,10 @@
 
-statuscheck() {
-  if [$? -eq 0];then
+StatusCheck() {
+  if [ $? -eq 0 ]; then
     echo -e "\e[32mSUCCESS\e[0m"
   else
-      echo -e "\e31mfailure\e[0m"
+    echo -e "\e[31mFAILURE\e[0m"
+    exit 1
   fi
 }
 
