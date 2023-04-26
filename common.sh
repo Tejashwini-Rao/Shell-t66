1034df41661b55ac7606b87934b9778e568e86bb
Tejashwini Rao <polepalle.07@gmail.com>
2023-04-26 20:36:49 +0530

    Pushing all the components

--- common.sh
+++ common.sh
@@ -10,7 +10,7 @@ statuscheck() {
 NodeJS() {
 
 echo Downloading nodejs dependencies
-curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash - &>>/tmp/${COMPONENT}.log
+curl --silent --location https://rpm.nodesource.com/setup_16.x | sudo bash &>>/tmp/${COMPONENT}.log
  yum install nodejs -y &>>/tmp/${COMPONENT}.log
  statuscheck
 
