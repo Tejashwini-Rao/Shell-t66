9c9fd6f57f738ff9ab08c97e94d29453d1d97b40
Tejashwini Rao <polepalle.07@gmail.com>
2023-04-27 18:29:08 +0530

    Pushing all the components

--- mysql.sh
+++ mysql.sh
@@ -35,7 +35,7 @@ if [ $? -eq 0 ]; then
 fi
 
 
-echo "Extract & Load Schema"
+echo Extract & Load Schema
  cd /tmp &>>/tmp/${COMPONENT}.log && unzip -o mysql.zip &>>/tmp/${COMPONENT}.log &&  cd mysql-main &>>/tmp/${COMPONENT}.log && mysql -u root -p$MYSQL_PASSWORD <shipping.sql &>>/tmp/${COMPONENT}.log
  statuscheck
 
