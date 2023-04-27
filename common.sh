2dbf2e769f9edc5db596b6a326ea02933252c4af
Tejashwini Rao <polepalle.07@gmail.com>
2023-04-27 20:32:16 +0530

    Pushing all the components

--- common.sh
+++ common.sh
@@ -57,8 +57,14 @@ Npm()
 
 Service(){
 
+  echo Update SystemD Config
+
+  sed -i -e 's/MONGO_DNSNAME/mongodb-dev.roboshop.internal/' -e 's/MONGO_ENDPOINT/mongodb-dev.roboshop.internal/' -e 's/REDIS_ENDPOINT/redis-dev.roboshop.internal/'
+  -e 's/CATALOGUE_ENDPOINT/catalogue-dev.roboshop.internal/' -e 's/AMQPHOST/rabbitmq-dev.roboshop.internal/' -e 's/CARTHOST/cart-dev.roboshop.internal/'
+  -e 's/USERHOST/user-dev.roboshop.internal/' -e 's/CARTENDPOINT/cart-dev.roboshop.internal/' -e 's/DBHOST/mysql-dev.roboshop.internal/' /home/roboshop/${COMPONENT}/systemd.service &>>/tmp/${COMPONENT}.log
+  StatusCheck
   echo moving  app content
-     mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service&>>/tmp/${COMPONENT}.log
+     mv /home/roboshop/${COMPONENT}/systemd.service /etc/systemd/system/${COMPONENT}.service &>>/tmp/${COMPONENT}.log
      statuscheck
 
   echo restarting the service
--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,28 @@
 #!/usr/bin/bash
-yum install nginx -y
-systemctl enable nginx
-systemctl start nginx
-curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip"
 
-cd /usr/share/nginx/html
+COMPONENT=frontend
+source common.sh
 
-rm -rf *
-unzip -o /tmp/frontend.zip
-mv frontend-main/static/* .
-mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
-systemctl restart ngin x
+echo Installing Nginx
+yum install nginx -y /tmp/${COMPONENT}.log
+StatusCheck
+
+DOWNLOAD
+
+echo Clean Old Content
+cd /usr/share/nginx/html && rm -rf *
+StatusCheck
+
+echo Extract Downloaded Content
+unzip -o /tmp/frontend.zip /tmp/${COMPONENT}.log && mv frontend-main/static/* . &&  mv frontend-main/localhost.conf /etc/nginx/default.d/roboshop.conf
+StatusCheck
+
+echo Updating Nginx Configuration
+sed -i -e '/catalogue/ s/localhost/catalogue-dev.roboshop.internal/' -e '/cart/ s/localhost/cart-dev.roboshop.internal/'
+-e '/user/ s/localhost/user-dev.roboshop.internal/' -e '/shipping/ s/localhost/shipping-dev.roboshop.internal/'
+ -e '/payment/ s/localhost/payment-dev.roboshop.internal/' /etc/nginx/default.d/roboshop.conf
+StatusCheck
+
+echo Start Nginx Service
+systemctl restart nginx /tmp/${COMPONENT}.log && systemctl enable nginx /tmp/${COMPONENT}.log
+StatusCheck
