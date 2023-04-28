db27e94f5c8cf4a7cbde8da0541e014f5da167ad
Tejashwini Rao <polepalle.07@gmail.com>
2023-04-28 19:50:36 +0530

    Pushing all the components

--- mysql.sh
+++ mysql.sh
@@ -15,25 +15,25 @@ echo start services
  systemctl enable mysqld &>>/tmp/${COMPONENT}.log && systemctl restart mysqld &>>/tmp/${COMPONENT}.log
  statuscheck
 
-echo "show databases;" | mysql -uroot -p$MYSQL_PASSWORD &>>/tmp/${COMPONENT}.log
+echo "show databases;" | mysql -uroot -p$RoboShop@1 &>>/tmp/${COMPONENT}.log
 if [ $? -ne 0 ]; then
   echo Changing Default Password
   DEFAULT_PASSWORD=$(grep 'A temporary password' /var/log/mysqld.log | awk '{print $NF}')
-  echo "alter user 'root'@'localhost' identified with mysql_native_password by '$MYSQL_PASSWORD';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>/tmp/${COMPONENT}.log
+  echo "alter user 'root'@'localhost' identified with mysql_native_password by 'RoboShop@1';" | mysql --connect-expired-password -uroot -p${DEFAULT_PASSWORD} &>>/tmp/${COMPONENT}.log
   statuscheck
 fi
 
 
-echo "show plugins;" | mysql -uroot -p$MYSQL_PASSWORD 2>&1 | grep validate_password &>>/tmp/${COMPONENT}.log
+echo "show plugins;" | mysql -uroot -p$RoboShop@1 2>&1 | grep validate_password &>>/tmp/${COMPONENT}.log
 if [ $? -eq 0 ]; then
   echo Remove Password Validate Plugin
-  echo "uninstall plugin validate_password;" | mysql -uroot -p$MYSQL_PASSWORD &>>/tmp/${COMPONENT}.log
+  echo "uninstall plugin validate_password;" | mysql -uroot -p$RoboShop@1 &>>/tmp/${COMPONENT}.log
   statuscheck
 fi
 
 
 echo "Extract & Load Schema"
- cd /tmp &>>/tmp/${COMPONENT}.log && unzip -o mysql.zip &>>/tmp/${COMPONENT}.log &&  cd mysql-main &>>/tmp/${COMPONENT}.log && mysql -u root -p$MYSQL_PASSWORD <shipping.sql &>>/tmp/${COMPONENT}.log
+ cd /tmp &>>/tmp/${COMPONENT}.log && unzip -o mysql.zip &>>/tmp/${COMPONENT}.log &&  cd mysql-main &>>/tmp/${COMPONENT}.log && mysql -u root -p$RoboShop@1 <shipping.sql &>>/tmp/${COMPONENT}.log
  statuscheck
 
 
